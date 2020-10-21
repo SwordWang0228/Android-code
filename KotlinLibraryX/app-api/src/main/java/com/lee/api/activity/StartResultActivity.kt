de0bd7da33097f37886aecc17f6268621e4b16c0
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-10-21 21:26:11 +0800

    添加window通信函数非空判断.

--- KotlinLibraryX/app-api/src/main/java/com/lee/api/activity/StartResultActivity.kt
+++ KotlinLibraryX/app-api/src/main/java/com/lee/api/activity/StartResultActivity.kt
@@ -1,11 +1,17 @@
 package com.lee.api.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.lee.api.R
 import com.lee.api.databinding.ActivityStartResultBinding
 import com.lee.library.base.BaseActivity
 import com.lee.library.mvvm.base.BaseViewModel
+import java.io.File
 
 class StartResultActivity :
     BaseActivity<ActivityStartResultBinding, BaseViewModel>(R.layout.activity_start_result) {
@@ -14,9 +20,36 @@ class StartResultActivity :
         toast(it?.data?.getStringExtra("value") ?: "")
     }
 
+    private val requestPermission = prepareCall(ActivityResultContracts.RequestPermission()) {
+        toast("request permission success")
+    }
+
+    private val requestPicture = prepareCall(ActivityResultContracts.TakePicture()) {
+        binding.ivPicture.setImageBitmap(it)
+    }
+
     override fun bindView() {
         binding.btnActivityResult.setOnClickListener {
             activityForResult.launch(Intent(this, ResultActivity::class.java))
+        }
+
+        binding.btnPictureResult.setOnClickListener {
+            //权限申请
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                requestPermission.launch(Manifest.permission.CAMERA)
+            }
+
+            val file =
+                File("${filesDir.absolutePath}${File.separator}${System.currentTimeMillis()}.jpg")
+
+            val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                FileProvider.getUriForFile(this, packageName.plus(".fileprovider"), file)
+            } else {
+                Uri.fromFile(file)
+            }
+
+            requestPicture.launch(uri)
+
         }
     }
 
